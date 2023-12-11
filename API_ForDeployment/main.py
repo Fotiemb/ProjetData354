3a8346e20e76703e82664d476de134c3839562c6
Fotiemb <fotiemb@gmail.com>
2023-12-11 18:00:44 +0000

    some modification

---
+++ API_ForDeployment/main.py
@@ -0,0 +1,26 @@
+from joblib import load
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Créer une instance FastAPI
+app = FastAPI()
+
+# Charger le modèle formé pour la classification des URL
+loaded_model = load('logreg.joblib')
+
+# Modèle de données pour la requête
+class UrlRequest(BaseModel):
+    url: str
+
+# Définir le point de terminaison
+@app.post("/predict-url")
+def predict_url(data: UrlRequest):
+    # Nouvelles données sur lesquelles on fait la prédiction
+    new_data = [[data.url]]
+
+    # Prédiction
+    prediction = loaded_model.predict(new_data)[0]
+
+    # Retourner le résultat de la prédiction
+    return {'prediction': prediction}
